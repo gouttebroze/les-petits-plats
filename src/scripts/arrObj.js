4be4004d06f54603be9790b326da2011999102a7
Hugues, programmer fullstack <docteurfutur@gmail.com>
2025-01-17 20:39:08 +0100

    add last treatement on others filters

--- src/scripts/arrObj.js
+++ src/scripts/arrObj.js
@@ -1,24 +1,3 @@
-function filterByReduceMethod(term) {
-  const recipesFiltered = recipes.reduce((acc, curr) => {
-    // acc. (or accumulator), curr. (or current value)
-    // const { id, ...otherProps } = curr; // destructuring recipes array, that is the curr. val. ("curr")
-    if (curr.name.toLowerCase().includes(term)
-      || curr.description.toLowerCase().includes(term)
-      || curr?.ingredients?.ingredient?.toLowerCase()?.includes(term)) {
-      acc.push(curr);
-      /* to each time it's processed, we push the elements into the acc., 
-      that is an array (because it start with initial value, that is an empty array)
-      */
-    }
-    return acc; /* return accumulator - it's operation works as follow: 
-        - each of the values in the table is processed, 
-        and each time it is processed, the value accumulates, 
-        starting with the initial value, & until acc. is returned...    
-    */
-  }, []); // initial value is an empty array []
-  return recipesFiltered;
-}
-
 /**
  * @typedef {Object} SearchQuery
  * @property {string} term - Le terme de recherche saisi par l'utilisateur.
@@ -51,6 +30,7 @@ function filterByReduce(searchForm) {
   // to advanced appliances search field,
   const appliancesFilter = recipesFiltered.reduce((acc, curr) => {
     if (curr?.appliance?.toLowerCase().includes(appliances)) {
+      // if (curr?.appliance?.every(app => app.includes(appliances))) {
       acc.push(curr); // return a new array with recipes that meet the condition  
     }
     return acc;
@@ -58,7 +38,9 @@ function filterByReduce(searchForm) {
 
   // to advanced ustensils search field 
   const ustensilsFilter = appliancesFilter.reduce((acc, curr) => {
-    if (curr.ustensils.some(ustensil => ustensil.toLowerCase().includes(ustensils))) {
+    if (ustensils.every(element => {
+      return curr.ustensils.some(ustensil => ustensil.toLowerCase().includes(element));
+    })) {
       acc.push(curr); // return a new array with recipes that meet the condition  
     }
     return acc;
@@ -66,7 +48,9 @@ function filterByReduce(searchForm) {
 
   // to advanced ingredient search field
   const ingredientsFilter = ustensilsFilter.reduce((acc, curr) => {
-    if (curr.ingredients.some(ing => ing.ingredient.toLowerCase().includes(ingredients))) {
+    if (ingredients.every(element => {
+      return curr.ingredients.some(ing => ing.ingredient.toLowerCase().includes(element))
+    })) {
       acc.push(curr); // return "acc", a new array with recipes that meet the condition  
     }
     return acc;
